4255ff85ae1035e425dc68d8c14e1d9a1a2dfce1
Max Körlinge <mkorlinge@hotmail.com>
2018-01-12 22:39:52 +0100

    more!

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -2,8 +2,8 @@ defmodule Bob do
   def hey(input) do
     cond do
       Regex.match?(~r/^\s*$/, input) -> "Fine. Be that way!"  
-      Regex.match?(~r/^[^a-z]+[^?]$/, input) -> "Whoa, chill out!"
       Regex.match?(~r/^.*\?$/, input) -> "Sure."  
+      String.upcase(input) == input && String.downcase(input) != input -> "Whoa, chill out!"
       true -> "Whatever."
     end
   end
