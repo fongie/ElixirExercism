922c1737945e939605ba66f6906ef8a9556b7803
Max Körlinge <mkorlinge@hotmail.com>
2018-01-28 10:32:03 +0100

    finished Env module for MetaInterpreter

---
+++ ElixirSeminars/MetaIntepreter/env.ex
@@ -0,0 +1,41 @@
+defmodule Env do
+
+  def new() do
+    []
+  end
+
+  def add(var, struct, env) do
+     [{var, struct} | env]
+  end
+
+  # Find id in environment, return {id, struct} if found or nil
+  def lookup(id, []) do
+    nil
+  end
+  def lookup(id, [{id, struct} | rest]) do
+    {id, struct}
+  end
+  def lookup(id, [{_, _} | rest]) do
+    lookup(id, rest)
+  end
+
+  # Remove all bindings of ids from env and return env without them
+  def remove([], env) do
+    env
+  end
+  def remove([id|rest], env) do
+    newenv = removeOne(id, env, [])
+    remove(rest, newenv)
+  end
+  def removeOne(id, [{id,_} | rest], newenv) do
+    removeOne(id, rest, newenv)
+  end
+  def removeOne(id, [h|t], newenv) do
+    removeOne(id, t, [h | newenv])
+  end
+
+  def removeOne(_, [], newenv) do
+    newenv
+  end
+
+end
---
+++ ElixirSeminars/MetaIntepreter/env_test.exs
@@ -0,0 +1,67 @@
+Code.load_file("env.ex", __DIR__)
+
+ExUnit.start
+ExUnit.configure exclude: :pending, trace: true
+
+defmodule EnvTest do
+  use ExUnit.Case
+
+  #@tag :pending
+  test "empty environment with new" do
+    assert Env.new() == []
+  end
+
+  #@tag :pending
+  test "add to empty environment" do
+    assert Env.add(:a, 42, Env.new()) == [{:a, 42}]
+  end
+
+  #@tag :pending
+  test "add to nonempty environment" do
+    assert Env.add(:b, 43, [{:a,42}]) == [{:b, 43}, {:a, 42}]
+  end
+
+  #@tag :pending
+  test "lookup in empty env" do
+    assert Env.lookup(:b, []) == nil
+  end
+
+  #@tag :pending
+  test "lookup existing var" do
+    assert Env.lookup(:a, [{:a,42},{:b,43}]) == {:a,42}
+  end
+
+  #@tag :pending
+  test "lookup nonexisting var" do
+    assert Env.lookup(:c, [{:a,42},{:b,43}]) == nil
+  end
+
+  #@tag :pending
+  test "remove no ids" do
+    assert Env.remove([], [{:a,41}]) == [{:a,41}]
+  end
+  #@tag :pending
+  test "remove nonexisting ids" do
+    assert Env.remove([:a], [{:b,41},{:c,40}]) == [{:c,40}, {:b,41}]
+  end
+
+  #@tag :pending
+  test "remove from empty env" do
+    assert Env.remove([:a], []) == []
+  end
+
+  #@tag :pending
+  test "remove one id" do
+    assert Env.remove([:a], [{:a,42},{:b,41}]) == [{:b,41}]
+  end
+
+  #@tag :pending
+  test "remove many ids" do
+    assert Env.remove([:a,:b], [{:a,42},{:b,41},{:c,40}]) == [{:c, 40}]
+  end
+
+  #@tag :pending
+  test "env test from instructions" do
+    assert Env.lookup(:foo, Env.add(:foo, 42, Env.new())) == {:foo, 42}
+  end
+end
